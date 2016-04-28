ae669a3e100ffc308b97ddacda66c5af7aabf53d
Ankur Srivastava <ankurlkw@gmail.com>
2016-04-28 07:32:40 +0530

    Added LocationListener

--- app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
+++ app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
@@ -1,15 +1,69 @@
 package com.edocent.surveyapp;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 
 public class DistanceTraveledService extends Service {
 
     DistanceTravelBinder mDistanceTravelBinder = new DistanceTravelBinder();
+    static double distanceInMetres;
+    static Location lastLocation = null;
 
     public DistanceTraveledService() {
+    }
+
+    @Override
+    public void onCreate() {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMetres += location.distanceTo(lastLocation);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000,
+                1,
+                locationListener);
     }
 
     @Override
