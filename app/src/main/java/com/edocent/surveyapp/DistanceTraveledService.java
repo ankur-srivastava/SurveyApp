6f016b3a39d60c57e3ccca429cee9fec6e136384
Ankur Srivastava <ankurlkw@gmail.com>
2016-04-29 08:27:00 +0530

    Added Service Connection

--- app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
+++ app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
@@ -49,18 +49,18 @@ public class DistanceTraveledService extends Service {
             }
         };
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
                 1000,
                 1,
                 locationListener);
@@ -73,8 +73,8 @@ public class DistanceTraveledService extends Service {
     }
 
     public class DistanceTravelBinder extends Binder{
-        DistanceTravelBinder getBinder(){
-            return DistanceTravelBinder.this;
+        DistanceTraveledService getBinder(){
+            return DistanceTraveledService.this;
         }
     }
 
--- app/src/main/java/com/edocent/surveyapp/MainActivity.java
+++ app/src/main/java/com/edocent/surveyapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.edocent.surveyapp;
 
+import android.content.ComponentName;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,11 +29,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    DistanceTraveledService mDistanceTraveledService;
+    boolean bound = false;
+
+    ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            DistanceTraveledService.DistanceTravelBinder distanceTravelBinder = (DistanceTraveledService.DistanceTravelBinder)service;
+            mDistanceTraveledService = distanceTravelBinder.getBinder();
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
+
     EditText userNameId;
     EditText userEmailId;
     EditText userAgeId;
     Button submitDataId;
     Button serviceId;
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, DistanceTraveledService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(bound){
+            unbindService(mServiceConnection);
+            bound = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
