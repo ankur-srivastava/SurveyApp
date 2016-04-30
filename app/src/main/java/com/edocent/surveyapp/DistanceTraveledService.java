298807c7c78ecacd7e433f3559b6c37cbcb970ac
Ankur Srivastava <ankurlkw@gmail.com>
2016-04-30 08:15:16 +0530

    Changed Main Layout & Return Binder

--- app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
+++ app/src/main/java/com/edocent/surveyapp/DistanceTraveledService.java
@@ -68,8 +68,7 @@ public class DistanceTraveledService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mDistanceTravelBinder;
     }
 
     public class DistanceTravelBinder extends Binder{
