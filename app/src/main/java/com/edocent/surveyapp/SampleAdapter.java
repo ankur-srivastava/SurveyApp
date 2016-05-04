7506ca2b4abe8c52cb64cc3348b615139f325c71
Ankur Srivastava <ankurlkw@gmail.com>
2016-05-04 08:22:07 +0530

    Added a sample adapter

---
+++ app/src/main/java/com/edocent/surveyapp/SampleAdapter.java
@@ -0,0 +1,26 @@
+package com.edocent.surveyapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by ankursrivastava on 5/4/16.
+ */
+public class SampleAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
