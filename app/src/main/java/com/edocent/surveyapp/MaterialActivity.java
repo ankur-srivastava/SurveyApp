ac0ea87cc740550a7b5d2b786e0ccbdcce03aeda
Ankur Srivastava <ankurlkw@gmail.com>
2016-05-07 08:43:08 +0530

    Added a new MaterialActivity

---
+++ app/src/main/java/com/edocent/surveyapp/MaterialActivity.java
@@ -0,0 +1,26 @@
+package com.edocent.surveyapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_material);
+
+        String[] inputData = {"Hello Sam", "Hello John", "Hello Riki"};
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.sample_recycler);
+
+        SampleAdapter adapter = new SampleAdapter(inputData);
+        recyclerView.setAdapter(adapter);
+    }
+
+}
