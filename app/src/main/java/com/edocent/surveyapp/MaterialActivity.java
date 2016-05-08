1a94705d5ad1407d30b4bb436c689c3cd3b6bb1d
Ankur Srivastava <ankurlkw@gmail.com>
2016-05-08 08:38:59 +0530

    Added Layout Manager

--- app/src/main/java/com/edocent/surveyapp/MaterialActivity.java
+++ app/src/main/java/com/edocent/surveyapp/MaterialActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,6 +23,9 @@ public class MaterialActivity extends AppCompatActivity {
 
         SampleAdapter adapter = new SampleAdapter(inputData);
         recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
     }
 
 }
